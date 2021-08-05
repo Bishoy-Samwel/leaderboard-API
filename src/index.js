b345a782e4e027a2c74c95fa9f8b112d8d7b63ff
bishoy-samwel <bishoy-samwuel@gmail.com>
2021-08-05 12:22:25 +0200

    Add refresh-btn functionality to retrieve data

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
 import './style.css';
+
+const scoresDiv = document.querySelector('#scores');
+const refreshBtn = document.querySelector('#refresh-btn');
+const submitBtn = document.querySelector('#refresh-btn');
+const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/Zl4d7IVkemOTTVg2fUdz/scores/';
+
+const getScores = async () => {
+  const request = await fetch(url);
+  const data = await request.json();
+  return data;
+};
+
+const CreateScore = (item) => {
+  const scorePar = document.createElement('p');
+  scorePar.textContent = `"${item.user}: ${item.score}"`;
+  return scorePar;
+};
+
+const displayScores = async () => {
+  const ans = await getScores();
+  const scores = ans.result;
+  scores.forEach((item) => {
+    scoresDiv.appendChild(CreateScore(item));
+  });
+};
+
+refreshBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  scoresDiv.innerHTML = '';
+  displayScores();
+});
